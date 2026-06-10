208c0410443d56b198e51f13e8f2e55ca92fb523
bennnnnnyyyyyyyy <ben.arthur.wiz@gmail.com>
2026-06-10 17:42:37 +0300

    s

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+// Firebase configuration for Google Apps Script
+// This is a global configuration object accessible to all .gs files
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVBLnjdM4cV9vBwV27dl6bEc4ZqVjuFBw",
+  authDomain: "ggowcpredictor.firebaseapp.com",
+  projectId: "ggowcpredictor",
+  storageBucket: "ggowcpredictor.firebasestorage.app",
+  messagingSenderId: "126058028551",
+  appId: "1:126058028551:web:e60b6e211c3e2e56e154a2",
+  measurementId: "G-YQLEYQ386D"
+};
