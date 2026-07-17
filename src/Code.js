656608b7fd02f78aa28ca19a1e3b6908276d6a9e
bennnnnnyyyyyyyy <ben.arthur.wiz@gmail.com>
2026-07-17 17:08:26 +0300

    S

--- src/Code.js
+++
@@ -1,7 +0,0 @@
-
-// All .js files in src/ are concatenated and deployed together
-// Main entry points are defined in main.js:
-// - doGet(e): Handle GET requests
-// - doPost(e): Handle POST requests
-// - scheduledLiveScoresUpdate(): Called by Apps Script trigger
-// - scheduledLeaderboardUpdate(): Called by Apps Script trigger
